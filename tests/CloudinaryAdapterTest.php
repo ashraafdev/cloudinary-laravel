9ada4887e7509e8d46b7c9fce20ccd3b08d2fa68
Achraf Hebboul <hebboul.ashraf@gmail.com>
2024-02-20 22:13:53 +0100

    add tests folder, create CloudinaryAdapterTest

---
+++ tests/CloudinaryAdapterTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AshraafDev\Tests\CloudinaryLaravel;
+
+use Ashraafdev\CloudinaryLaravel;
+use PHPUnit\Framework\TestCase;
+
+class CloudinaryAdapterTest extends TestCase {
+    /* public function testTrueIsTrue() {
+        return $this->assertTrue(true);
+    } */
+}
+?>
